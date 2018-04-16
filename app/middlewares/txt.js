602d04f9001bc05abf69fbcc020e2749a4316fc5
jBox <junhua@outlook.com>
2018-04-16 22:11:23 +0800

    update txt middleware

--- app/middlewares/txt.js
+++ app/middlewares/txt.js
@@ -1,15 +1,19 @@
 const cv = require("config-vars");
 const Path = require("path");
-const fs = require("fs");
+const fs = require("fs-extra");
 
 module.exports = (req, res, next) => {
     const pathname = req.path;
     const [, txt] = Array.from(/\/(\w+.txt)$/ig.exec(pathname) || []);
     if (txt) {
         const filePath = Path.resolve(cv.env.shareFolder, `./${txt}`);
-        if (fs.existsSync(filePath)) {
-            return res.sendFile(filePath);
-        }
+        return fs.stat(filePath).then((stats) => {
+            if (stats.isFile()) {
+                return res.sendFile(filePath);
+            } else {
+                return next();
+            }
+        }).catch(() => next());
     }
 
     return next();
